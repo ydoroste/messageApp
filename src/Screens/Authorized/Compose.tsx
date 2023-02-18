65cd7f44eb1c4bcf53962ac6eb3067c7f9a55caf
Khaled Fares <kfares29@gmail.com>
2023-02-19 00:09:00 +0200

    Refactor compose state management

--- src/Screens/Authorized/Compose.tsx
+++ src/Screens/Authorized/Compose.tsx
@@ -1,6 +1,10 @@
 import IconButton from "@followBack/GenericElements/IconButton";
 import Typography from "@followBack/GenericElements/Typography";
+<<<<<<< Updated upstream
 import * as React from "react";
+=======
+import React, { useCallback, useReducer } from "react";
+>>>>>>> Stashed changes
 import {
   Keyboard,
   KeyboardAvoidingView,
@@ -20,23 +24,82 @@ import { ComposeAutoComplete } from "@followBack/Elements/ComposeAutoComplete";
 import { useCompose } from "@followBack/Hooks/Apis/Compose";
 import { IComposeApiRequest } from "@followBack/Apis/Compose/types";
 import { isValidEmail } from "@followBack/Utils/validations";
+<<<<<<< Updated upstream
 const Compose: React.FC = ({ navigation }) => {
   const [toSearchValue, setToSearchValue] = useState("");
   const [ccSearchValue, setcCSearchValue] = useState("");
   const [bccSearchValue, setBccSearchValue] = useState("");
+=======
+import { useFocusEffect } from "@react-navigation/native";
 
-  const [toTags, setToTags] = useState([]);
-  const [ccTags, setCcTags] = useState([]);
-  const [bccTags, setBccTags] = useState([]);
+const SET_TO_SEARCH_VALUE = "SET_TO_SEARCH_VALUE";
+const SET_CC_SEARCH_VALUE = "SET_CC_SEARCH_VALUE";
+const SET_BCC_SEARCH_VALUE = "SET_BCC_SEARCH_VALUE";
 
-  const [subject, setSubject] = useState("");
+const SET_SUBJECT = "SET_SUBJECT";
+const SET_MAIL = "SET_MAIL";
+const SET_TO_TAGS = "SET_TO_TAGS";
+const SET_CC_TAGS = "SET_CC_TAGS";
+const SET_BCC_TAGS = "SET_BCC_TAGS";
 
-  const [mail, setMail] = useState("");
+const SET_KEY_VALUE = "SET_KEY_VALUE";
+
+const RESET = "RESET";
+
+const initialState = {
+  toSearchValue: "",
+  ccSearchValue: "",
+  bccSearchValue: "",
+  toTags: [],
+  ccTags: [],
+  bccTags: [],
+  subject: "",
+  mail: "",
+  showSubject: false,
+  showCC: false,
+  showBcc: false,
+};
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case SET_KEY_VALUE:
+      return { ...state, [payload.key]: payload.value };
+
+    case RESET:
+      return initialState;
+
+    default:
+      return state;
+  }
+};
+
+const Compose: React.FC = ({ navigation }) => {
+  const { inboxThread } = useMailBoxes();
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {
+    toSearchValue,
+    ccSearchValue,
+    bccSearchValue,
+    toTags,
+    ccTags,
+    bccTags,
+    mail,
+    subject,
+    showSubject,
+    showCC,
+    showBcc,
+  } = state;
+>>>>>>> Stashed changes
+
+  const setKeyValue = ({ key, value }) => {
+    dispatch({
+      type: SET_KEY_VALUE,
+      payload: { key, value },
+    });
+  };
 
   const { colors } = useTheme();
-  const [showSubject, setShowSubject] = useState(false);
-  const [showCC, setShowCC] = useState(false);
-  const [showBcc, setShowBcc] = useState(false);
 
   const formatTags = (tags) => tags.map((mail) => ({ address: mail.trim() }));
   const formattedToTags = formatTags(toTags);
@@ -57,11 +120,30 @@ const Compose: React.FC = ({ navigation }) => {
       : formattedBccTags,
   };
 
+<<<<<<< Updated upstream
   const { refetch } = useCompose(composeRequest);
   const onPressCompose = async () => {
     if (!subject || toTags.length < 0) return;
     await refetch();
 
+=======
+  const reset = () => {
+    dispatch({ type: RESET, payload: null });
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        reset();
+      };
+    }, [])
+  );
+
+  const { refetch } = useCompose(composeRequest);
+  const onPressCompose = async () => {
+    if (!subject || toTags.length < 0) return;
+    const { data } = await refetch();
+>>>>>>> Stashed changes
     navigation.navigate(AuthorizedScreensEnum.composeStack, {
       screen: AuthorizedScreensEnum.threadDetails,
       params: {},
@@ -76,6 +158,7 @@ const Compose: React.FC = ({ navigation }) => {
       <Pressable onPress={Keyboard.dismiss} style={styles.container}>
         <View>
           <View style={styles.header}>
+<<<<<<< Updated upstream
             <Button
               icon={() => (
                 <View style={{ marginLeft: -13 }}>
@@ -84,6 +167,65 @@ const Compose: React.FC = ({ navigation }) => {
               )}
               type="mediumTernary"
             >
+=======
+            <View style={styles.leftHeaderActions}>
+              <View style={styles.back}>
+                <IconButton
+                  name="back"
+                  onPress={() => {
+                    if (navigation.canGoBack()) return navigation.goBack();
+                    if (!inboxThread) return;
+
+                    navigation.navigate(
+                      AuthorizedScreensEnum.threadsListStack,
+                      {
+                        screen: AuthorizedScreensEnum.threadsList,
+                        params: { ...inboxThread },
+                      }
+                    );
+                  }}
+                  color={colors.grey02}
+                  width={10}
+                  height={16}
+                />
+              </View>
+
+              <View style={styles.mailActions}>
+                <View style={styles.subject}>
+                  <Button
+                    onPress={() =>
+                      setKeyValue({ key: "showSubject", value: !showSubject })
+                    }
+                    type="mediumTernary"
+                  >
+                    subject
+                  </Button>
+                </View>
+                <View>
+                  <Button
+                    onPress={() =>
+                      setKeyValue({ key: "showCC", value: !showCC })
+                    }
+                    type="mediumTernary"
+                  >
+                    cc/
+                  </Button>
+                </View>
+                <View>
+                  <Button
+                    onPress={() =>
+                      setKeyValue({ key: "showBcc", value: !showBcc })
+                    }
+                    type="mediumTernary"
+                  >
+                    bcc
+                  </Button>
+                </View>
+              </View>
+            </View>
+
+            <Button onPress={reset} style={styles.delete} type="mediumTernary">
+>>>>>>> Stashed changes
               delete
             </Button>
 
@@ -125,7 +267,9 @@ const Compose: React.FC = ({ navigation }) => {
                     hideBorder
                     placeholder="add"
                     value={subject}
-                    onChangeText={setSubject}
+                    onChangeText={(text) =>
+                      setKeyValue({ key: "subject", value: text })
+                    }
                   />
                 </View>
               </View>
@@ -139,9 +283,13 @@ const Compose: React.FC = ({ navigation }) => {
                 <View style={styles.input}>
                   <ComposeAutoComplete
                     searchValue={bccSearchValue}
-                    setSearchValue={setBccSearchValue}
+                    setSearchValue={(text) =>
+                      setKeyValue({ key: "bccSearchValue", value: text })
+                    }
                     tags={bccTags}
-                    setTags={setBccTags}
+                    setTags={(tags) =>
+                      setKeyValue({ key: "bccTags", value: tags })
+                    }
                     type={"bcc"}
                   />
                 </View>
@@ -155,9 +303,13 @@ const Compose: React.FC = ({ navigation }) => {
                 <View style={styles.input}>
                   <ComposeAutoComplete
                     searchValue={ccSearchValue}
-                    setSearchValue={setcCSearchValue}
+                    setSearchValue={(text) =>
+                      setKeyValue({ key: "ccSearchValue", value: text })
+                    }
                     tags={ccTags}
-                    setTags={setCcTags}
+                    setTags={(tags) =>
+                      setKeyValue({ key: "ccTags", value: tags })
+                    }
                     type={"cc"}
                   />
                 </View>
@@ -170,9 +322,13 @@ const Compose: React.FC = ({ navigation }) => {
               <View style={styles.input}>
                 <ComposeAutoComplete
                   searchValue={toSearchValue}
-                  setSearchValue={setToSearchValue}
+                  setSearchValue={(text) =>
+                    setKeyValue({ key: "toSearchValue", value: text })
+                  }
                   tags={toTags}
-                  setTags={setToTags}
+                  setTags={(tags) =>
+                    setKeyValue({ key: "toTags", value: tags })
+                  }
                   type={"to"}
                 />
               </View>
@@ -190,7 +346,7 @@ const Compose: React.FC = ({ navigation }) => {
           <View style={styles.input}>
             <InputField
               value={mail}
-              onChangeText={setMail}
+              onChangeText={(mail) => setKeyValue({ key: "mail", value: mail })}
               multiline
               mode="outlined"
               placeholder="write a message..."
