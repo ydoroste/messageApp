68a6208c51c688f0304c303e30d6792331d25fe0
tarek <tarek.morshid@gmail.com>
2023-04-17 15:43:32 +0200

    Tarek changes

--- src/Elements/Message/Message.tsx
+++ src/Elements/Message/Message.tsx
@@ -1,23 +1,38 @@
 import Typography from "@followBack/GenericElements/Typography";
-import React, {useRef, useState} from "react";
-import {View, StyleSheet, Pressable, ViewStyle} from "react-native";
-import {formatMessageDate} from "@followBack/Utils/date";
+import React, { useRef, useState } from "react";
+import { View, StyleSheet, Pressable, ViewStyle } from "react-native";
+import { formatMessageDate } from "@followBack/Utils/date";
 import useStylesWithTheme from "@followBack/Hooks/useStylesWithTheme";
 import { useUserDetails } from "@followBack/Hooks/useUserDetails";
 import { excludeUser } from "@followBack/Utils/messages";
 import { emailNameParcer } from "@followBack/Utils/email";
-import {Gesture, GestureDetector} from "react-native-gesture-handler";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 const Message = ({ item }) => {
   const { styles } = useStyles();
   const { text, to, from, cc, bcc, messageDateTime } = item;
   const { userDetails } = useUserDetails();
-  console.log("console",!item?.from?.address ? true : userDetails.user_name === emailNameParcer(item?.from?.address))
-  const isOwnMessage =  !item?.from?.address ? true : userDetails.user_name === emailNameParcer(item?.from?.address);
+  console.log(
+    "console",
+    !item?.from?.address
+      ? true
+      : userDetails.user_name === emailNameParcer(item?.from?.address)
+  );
+  const isOwnMessage = !item?.from?.address
+    ? true
+    : userDetails.user_name === emailNameParcer(item?.from?.address);
 
-  const chatUsers = [...to, ...cc, ...bcc, from];
-    const itemPosition = useRef<number>(0);
-    const [showDate, setShowDate] = useState(false);
+  const sender = from ?? {
+    name: "t m",
+    address: userDetails.email,
+  };
+  const to_list = to ?? [];
+  const cc_list = cc ?? [];
+  const bcc_list = bcc ?? [];
+
+  const chatUsers = [...to_list, ...cc_list, ...bcc_list, sender];
+  const itemPosition = useRef<number>(0);
+  const [showDate, setShowDate] = useState(false);
 
   const others = excludeUser({
     users: chatUsers,
@@ -25,72 +40,90 @@ const Message = ({ item }) => {
   });
 
   const isGroupChat = others.length > 1;
-  const messageSender = from;
+  const messageSender = sender;
   const messageSenderLabel =
     messageSender.name.length > 0 ? messageSender.name : messageSender.address;
 
-    const contentContainerStyles = isGroupChat
-        ? [styles.contentContainer]
-        : [styles.contentContainer, styles.groupContentContainer];
-    return (
-        <>
-
-            <View onLayout={(event) => {
-                const {height} = event.nativeEvent.layout;
-                itemPosition.current = (height / 2) - 4;
-            }} style={{...styles.container,...(isOwnMessage ?{ marginLeft: "auto"} : {marginRight: "auto"})}}>
-                <Pressable onPress={() => {
-                    setShowDate(prevState => !prevState)
-                }} style={contentContainerStyles}>
-                    {isGroupChat && (
-                        <Typography type="largeBoldBody" color="chat">
-                            {messageSenderLabel + " "}
-                        </Typography>
-                    )}
-                    <Typography type="largeRegularBody" color="chat">
-                        {text}
-                    </Typography>
-
-                </Pressable>
-            </View>
-            {showDate && <View style={[styles.date, {top: itemPosition.current,  left: isOwnMessage ? 0 : undefined, right: !isOwnMessage ? 0 : undefined}]}>
-                <Typography type="smallRegularBody" color="secondary">
-                    {formatMessageDate(messageDateTime)}
-                </Typography>
-            </View>}
-
-        </>
-    );
+  const contentContainerStyles = isGroupChat
+    ? [styles.contentContainer]
+    : [styles.contentContainer, styles.groupContentContainer];
+  return (
+    <>
+      <View
+        onLayout={(event) => {
+          const { height } = event.nativeEvent.layout;
+          itemPosition.current = height / 2 - 4;
+        }}
+        style={{
+          ...styles.container,
+          ...(isOwnMessage ? { marginLeft: "auto" } : { marginRight: "auto" }),
+        }}
+      >
+        <Pressable
+          onPress={() => {
+            setShowDate((prevState) => !prevState);
+          }}
+          style={contentContainerStyles}
+        >
+          {isGroupChat && (
+            <Typography type="largeBoldBody" color="chat">
+              {messageSenderLabel + " "}
+            </Typography>
+          )}
+          <Typography type="largeRegularBody" color="chat">
+            {text}
+          </Typography>
+        </Pressable>
+      </View>
+      {showDate && (
+        <View
+          style={[
+            styles.date,
+            {
+              top: itemPosition.current,
+              left: isOwnMessage ? 0 : undefined,
+              right: !isOwnMessage ? 0 : undefined,
+            },
+          ]}
+        >
+          <Typography type="smallRegularBody" color="secondary">
+            {formatMessageDate(messageDateTime)}
+          </Typography>
+        </View>
+      )}
+    </>
+  );
 };
 
 const useStyles = useStylesWithTheme((theme) => ({
-    container: {
-        // paddingVertical: 5
-        //marginBottom: 16
-        position: "relative"
-    },
-    date: {
-        marginBottom: 12,
-        flexDirection: "row",
-        justifyContent: "center",
-        textAlign: "center",
-        position: "absolute",
-    },
-    contentContainer: {
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        width: "80%",
-        borderRadius: 20,
-        backgroundColor: theme.colors.dark02,
-    },
+  container: {
+    // paddingVertical: 5
+    //marginBottom: 16
+    position: "relative",
+  },
+  date: {
+    marginBottom: 12,
+    flexDirection: "row",
+    justifyContent: "center",
+    textAlign: "center",
+    position: "absolute",
+  },
+  contentContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    width: "80%",
+    borderRadius: 20,
+    backgroundColor: theme.colors.dark02,
+  },
 
-    groupContentContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-    }
+  groupContentContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
 }));
 
 export default React.memo(
   Message,
-  (prevProps, nextProps) => JSON.stringify(prevProps) === JSON.stringify(nextProps)
+  (prevProps, nextProps) =>
+    JSON.stringify(prevProps) === JSON.stringify(nextProps)
 );
--- src/Screens/Authorized/ThreadDetails.tsx
+++ src/Screens/Authorized/ThreadDetails.tsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { View, KeyboardAvoidingView, Platform, StyleSheet, TouchableHighlight, Pressable, Keyboard, FlatList } from "react-native";
+import {
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TouchableHighlight,
+  Pressable,
+  Keyboard,
+  FlatList,
+} from "react-native";
 import useTheme from "@followBack/Hooks/useTheme";
 import { useFetchThreadMessages } from "@followBack/Hooks/Apis/ThreadMessages";
 import Message from "@followBack/Elements/Message/Message";
@@ -27,20 +36,38 @@ const ThreadDetails: React.FC = ({ navigation, options, route }) => {
   const { userDetails } = useUserDetails();
   const onChangeMailContent = ({ value }) => setMail(value);
   const [refetchData, setRefetchData] = useState(false);
-  const [lastMessageData, setLastMessageData] = useState({})
-  const { data, isLoading, isError, isSuccess, hasNextPage, fetchNextPage } =
-    useFetchThreadMessages({ id, refetchData });
+  const [lastMessageData, setLastMessageData] = useState({});
+  const {
+    data,
+    isLoading,
+    isError,
+    isSuccess,
+    hasNextPage,
+    fetchNextPage,
+  } = useFetchThreadMessages({ id, refetchData });
 
   const hasData = allMessages?.length > 0;
   const firstMessage = allMessages?.[0];
-  const others = hasData && firstMessage ? excludeUser({
-    users: [firstMessage?.from, ...firstMessage?.to, ...firstMessage?.cc, ...firstMessage?.bcc],
-    userAddress: userDetails.email,
-  }) : [];
+  const sender = firstMessage?.from ?? {
+    name: "t m",
+    address: userDetails.email,
+  };
+  const to = firstMessage?.to ?? [];
+  const cc = firstMessage?.cc ?? [];
+  const bcc = firstMessage?.bcc ?? [];
+  console.log(sender);
+  const others =
+    hasData && firstMessage
+      ? excludeUser({
+          users: [sender, ...to, ...cc, ...bcc],
+          userAddress: userDetails.email,
+        })
+      : [];
   const receiver = hasData ? getThreadParticipantsUserName(others) : "";
   const subject = hasData ? firstMessage?.subject : "";
-  const firstMessageDate = hasData ? formatMessageDate(firstMessage?.messageDateTime) : "";
-
+  const firstMessageDate = hasData
+    ? formatMessageDate(firstMessage?.messageDateTime)
+    : "";
 
   const loadNextPageData = async () => {
     if (hasNextPage) {
@@ -48,24 +75,23 @@ const ThreadDetails: React.FC = ({ navigation, options, route }) => {
     }
   };
   const MenuItems = [
-    { text: 'unsend', onPress: () => { } },
-    { text: 'edit', onPress: () => { } },
+    { text: "unsend", onPress: () => {} },
+    { text: "edit", onPress: () => {} },
   ];
 
   useEffect(() => {
     if (!data || !data?.pages || data?.pages?.[0] === undefined) {
       return;
-    };
-    const flattenData = !!data?.pages && data?.pages?.[0] !== undefined
-      ? data?.pages.flatMap((page) => page?.data)
-      : [];
+    }
+    const flattenData =
+      !!data?.pages && data?.pages?.[0] !== undefined
+        ? data?.pages.flatMap((page) => page?.data)
+        : [];
     setAllMessages(flattenData);
     console.log("others", others);
     console.log("others", flattenData);
     setLastMessageData(flattenData[flattenData?.length - 1]);
-
   }, [data]);
-
 
   useFocusEffect(
     useCallback(() => {
@@ -76,14 +102,20 @@ const ThreadDetails: React.FC = ({ navigation, options, route }) => {
     }, [])
   );
 
-  const formatEndPoints = (endPoint: { address: string, name: string }[]): { address: string }[] => endPoint?.map((obj) => ({ address: obj?.address?.trim() }))
-  .filter(({address}) => address !==  userDetails.email);
+  const formatEndPoints = (
+    endPoint: { address: string; name: string }[]
+  ): { address: string }[] =>
+    endPoint
+      ?.map((obj) => ({ address: obj?.address?.trim() }))
+      .filter(({ address }) => address !== userDetails.email);
 
   const createComposeRequest = (): IComposeApiRequest => {
     const lastFromEndPoint = lastMessageData?.from?.address || "";
-    const toEndPoints = formatEndPoints(lastMessageData?.to)?.filter(({address})=> address !== lastFromEndPoint);
-    if(lastFromEndPoint !== userDetails.email){
-      toEndPoints?.push({address: lastFromEndPoint})
+    const toEndPoints = formatEndPoints(lastMessageData?.to)?.filter(
+      ({ address }) => address !== lastFromEndPoint
+    );
+    if (lastFromEndPoint !== userDetails.email) {
+      toEndPoints?.push({ address: lastFromEndPoint });
     }
     const composeRequest: IComposeApiRequest = {
       subject: lastMessageData?.subject,
@@ -92,25 +124,28 @@ const ThreadDetails: React.FC = ({ navigation, options, route }) => {
       cc: formatEndPoints(lastMessageData?.cc || []),
       bcc: formatEndPoints(lastMessageData?.bcc || []),
       from: userDetails?.email,
-      uid: lastMessageData?.uid
+      uid: lastMessageData?.uid,
     };
 
-    return composeRequest
+    return composeRequest;
   };
   const { refetch: recallComposeApi } = useCompose(createComposeRequest());
   const onPressCompose = async () => {
     const { data } = await recallComposeApi();
-    console.log("data", data)
+    console.log("data", data);
     if (data?.["success"]) {
-      setMail("")
+      setMail("");
     }
   };
 
   if (!hasData || isError) {
-    return(
-      <LoadingScreen loadingText={isError ? "Something Wrong" : "Loading"} loadingIndecatorSize={20}/>
-    )
-  };
+    return (
+      <LoadingScreen
+        loadingText={isError ? "Something Wrong" : "Loading"}
+        loadingIndecatorSize={20}
+      />
+    );
+  }
 
   return (
     <KeyboardAvoidingView
@@ -119,7 +154,6 @@ const ThreadDetails: React.FC = ({ navigation, options, route }) => {
       style={{ flex: 1, backgroundColor: colors.black }}
     >
       <View style={styles.container}>
-
         {hasData && (
           <ThreadDetailsHeader
             receiver={receiver}
@@ -131,28 +165,28 @@ const ThreadDetails: React.FC = ({ navigation, options, route }) => {
 
         <View style={styles.chatWrapper}>
           {hasData && (
-              <>
-            <FlatList
-              data={allMessages}
-              ItemSeparatorComponent={() => <View style={{height: 16}} />}
-
-              renderItem={({ item }) => <HoldItem  items={MenuItems}>
-                  <Message item={item} />
-              </HoldItem>}
-              keyExtractor={(item) => item?.messageId}
-              onStartReached={loadNextPageData}
-              showDefaultLoadingIndicators={true}
-              estimatedItemSize={20}
-              onStartReachedThreshold={10}
-              onEndReachedThreshold={10}
-              activityIndicatorColor={"black"}
-              enableAutoscrollToTop={false}
-              inverted={true}
-            />
-                <View style={{height: 30}} />
+            <>
+              <FlatList
+                data={allMessages}
+                ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+                renderItem={({ item }) => (
+                  <HoldItem items={MenuItems}>
+                    <Message item={item} />
+                  </HoldItem>
+                )}
+                keyExtractor={(item) => item?.messageId}
+                onStartReached={loadNextPageData}
+                showDefaultLoadingIndicators={true}
+                estimatedItemSize={20}
+                onStartReachedThreshold={10}
+                onEndReachedThreshold={10}
+                activityIndicatorColor={"black"}
+                enableAutoscrollToTop={false}
+                inverted={true}
+              />
+              <View style={{ height: 30 }} />
             </>
           )}
-
         </View>
         <MailSender
           mail={mail}
@@ -160,7 +194,6 @@ const ThreadDetails: React.FC = ({ navigation, options, route }) => {
           onPressCompose={onPressCompose}
         />
       </View>
-
     </KeyboardAvoidingView>
   );
 };
@@ -170,22 +203,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
-    marginTop: Platform.OS === "ios" ? 20 : 0
+    marginTop: Platform.OS === "ios" ? 20 : 0,
   },
   titleText: {
     height: 25,
-
   },
   chatWrapper: {
     flex: 1,
-    marginBottom: 48
+    marginBottom: 48,
   },
   emptyOrErrorMessageContainer: {
     alignItems: "center",
     flex: 1,
     height: "100%",
     backgroundColor: "black",
-    paddingTop: 50
-  }
-
+    paddingTop: 50,
+  },
 });
